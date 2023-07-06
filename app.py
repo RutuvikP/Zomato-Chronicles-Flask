405741bd5f072f67f1fedb3da1c1ef5085e1794d
Rutuvik Patil <you@example.com>
2023-07-06 09:46:21 +0530

    Routes Updated

--- app.py
+++ app.py
@@ -1,81 +1,53 @@
 import json
 from flask_cors import CORS
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, request, jsonify
+import ssl
+import pymongo
+from pymongo import MongoClient
+from bson import json_util
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-# Load menu data from menu.json
-with open('menu.json', 'r') as f:
-    menu = json.load(f)
-
-# Load orders data from orders.json
-with open('orders.json', 'r') as f:
-    orders = json.load(f)
-
-# Load users data from users.json
-with open('users.json', 'r') as f:
-    users = json.load(f)
+# Connect to MongoDB Atlas
+client = MongoClient("mongodb+srv://rutuvik:patil@cluster0.yalls5i.mongodb.net/flavorfusion?retryWrites=true&w=majority",ssl=True,ssl_cert_reqs=ssl.CERT_NONE)
+db = client.get_database("flavorfusion")
+menu_collection = db.menu
+orders_collection = db.orders
+users_collection = db.users
 
 def get_dish_by_id(dish_id):
-    for dish in menu:
-        if dish['id'] == dish_id:
-            return dish
-    return None
+    return menu_collection.find_one({"id": dish_id})
 
 def update_dish_availability(dish_id, availability):
-    dish = get_dish_by_id(dish_id)
-    if dish:
-        dish['availability'] = availability
-        return True
-    return False
+    result = menu_collection.update_one({"id": dish_id}, {"$set": {"availability": availability}})
+    return result.modified_count > 0
 
 def get_order_by_id(order_id):
-    for order in orders:
-        if order['id'] == order_id:
-            return order
-    
-    return None
+    return orders_collection.find_one({"id": order_id})
 
-def add_order(customer_name, dish_ids):
-    order_id = len(orders) + 1
-    order = {'id': order_id, 'customer_name': customer_name, 'dish_ids': dish_ids, 'status': 'received'}
-    orders.append(order)
-
-    # Save updated orders data to orders.json
-    with open('orders.json', 'w') as f:
-        json.dump(orders, f, indent=4)
-
+def add_order(customer_name, dish_ids, customer_email):
+    order_id = orders_collection.count_documents({}) + 1
+    order = {'id': order_id, 'customer_name': customer_name, 'customer_email':customer_email, 'dish_ids': dish_ids, 'status': 'received'}
+    orders_collection.insert_one(order)
     return order_id
 
 def update_order_status(order_id, status):
-    for order in orders:
-        if order['id'] == order_id:
-            order['status'] = status
-
-            # Save updated orders data to orders.json
-            with open('orders.json', 'w') as f:
-                json.dump(orders, f, indent=4)
-
-            return True
-    return False
+    result = orders_collection.update_one({"id": order_id}, {"$set": {"status": status}})
+    return result.modified_count > 0
 
 def get_user_by_email(email):
-    for user in users:
-        if user['email'] == email:
-            return user
-    return None
+    return users_collection.find_one({"email": email})
 
 def create_user(email, password, role):
-    user_id = len(users) + 1
-    user = {'id': user_id, 'email': email, 'password': password, 'role':role}
-    users.append(user)
-
-    # Save updated users data to users.json
-    with open('users.json', 'w') as f:
-        json.dump(users, f, indent=4)
-
+    user_id = users_collection.count_documents({}) + 1
+    user = {'id': user_id, 'email': email, 'password': password, 'role': role}
+    users_collection.insert_one(user)
     return user_id
+
+def generate_response(data=None, message=None, error=None, status_code=200):
+    response = {'data': data, 'message': message, 'error': error}
+    return jsonify(response), status_code
 
 @app.route('/signup', methods=['POST'])
 def signup():
@@ -86,22 +58,13 @@ def signup():
         role = data['role']
         
         # Check if the user with the given email already exists
-        with open('users.json', 'r') as f:
-            users = json.load(f)
-            for user in users:
-                if user['email'] == email:
-                    return jsonify(error='User with this email already exists')
+        existing_user = get_user_by_email(email)
+        if existing_user:
+            return generate_response(error='User with this email already exists', status_code=400)
 
         # Create a new user
-        user_id = len(users) + 1
-        user = {'id': user_id, 'email': email, 'password': password, 'role':role}
-        users.append(user)
-
-        # Save updated users data to users.json
-        with open('users.json', 'w') as f:
-            json.dump(users, f, indent=4)
-
-        return jsonify(message='User signed up successfully')
+        user_id = create_user(email, password, role)
+        return generate_response(message='User signed up successfully')
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -111,106 +74,85 @@ def login():
         password = data['password']
 
         # Check if the user with the given email and password exists
-        with open('users.json', 'r') as f:
-            users = json.load(f)
-            for user in users:
-                if user['email'] == email and user['password'] == password:
-                    return jsonify(message='User logged in successfully', role=user['role'])
+        user = get_user_by_email(email)
+        if user and user['password'] == password:
+            return generate_response(message='User logged in successfully', data={'role': user['role'],'email':user['email']})
 
         # If no matching user found, return an error message
-        return jsonify(error='Invalid email or password')
+        return generate_response(error='Invalid email or password', status_code=401)
 
-# Route for displaying the menu
 @app.route('/')
 def display_menu():
-    return jsonify(menu=menu)
+    menu = list(menu_collection.find())
+    menu_json = json.dumps(menu, default=json_util.default)
+    menu_dict = json.loads(menu_json)
+    return generate_response(data={'menu': menu_dict})
 
-# Route for adding a new dish to the menu
 @app.route('/menu/add', methods=['POST'])
 def add_dish():
     if request.method == 'POST':
         data = request.get_json()
-        dish_id = len(menu) + 1
+        dish_id = menu_collection.count_documents({}) + 1
         dish_name = data['name']
         price = int(data['price'])
         image = data['image']
         availability = data.get('availability', False)
         dish = {'id': dish_id, 'name': dish_name, 'price': price, 'image': image, 'availability': availability}
-        menu.append(dish)
+        menu_collection.insert_one(dish)
+        return generate_response(message='Dish added successfully')
 
-        # Save updated menu data to menu.json
-        with open('menu.json', 'w') as f:
-            json.dump(menu, f, indent=4)
-
-        return jsonify(message='Dish added successfully')
-
-# Route for removing a dish from the menu
 @app.route('/menu/remove/<int:dish_id>', methods=['DELETE'])
 def remove_dish(dish_id):
-    dish = get_dish_by_id(dish_id)
-    if dish:
-        menu.remove(dish)
-
-        # Save updated menu data to menu.json
-        with open('menu.json', 'w') as f:
-            json.dump(menu, f, indent=4)
-
-        return jsonify(message='Dish removed successfully')
+    result = menu_collection.delete_one({"id": dish_id})
+    if result.deleted_count > 0:
+        return generate_response(message='Dish removed successfully')
     else:
-        return jsonify(error='Invalid dish ID')
+        return generate_response(error='Invalid dish ID', status_code=400)
 
-# Route for updating the availability of a dish
 @app.route('/menu/update_dish/<int:dish_id>', methods=['PUT'])
 def update_dish(dish_id):
     dish = get_dish_by_id(dish_id)
     if dish:
         data = request.get_json()
+        updates = {}
         if 'price' in data:
-            dish['price'] = data['price']
+            updates['price'] = int(data['price'])
         if 'availability' in data:
-            dish['availability'] = data['availability']
-
-        # Save updated menu data to menu.json
-        with open('menu.json', 'w') as f:
-            json.dump(menu, f, indent=4)
-
-        return jsonify(message='Dish updated successfully')
+            updates['availability'] = data['availability']
+        menu_collection.update_one({"id": dish_id}, {"$set": updates})
+        return generate_response(message='Dish updated successfully')
     else:
-        return jsonify(error='Invalid dish ID')
+        return generate_response(error='Invalid dish ID', status_code=400)
 
-
-# Route for taking a new order
 @app.route('/order/new', methods=['POST'])
 def new_order():
     if request.method == 'POST':
         data = request.get_json()
         customer_name = data['customer_name']
+        customer_email = data['customer_email']
         dish_ids = [int(dish_id.strip()) for dish_id in data['dish_ids'].split(',')]
 
         for dish_id in dish_ids:
             dish = get_dish_by_id(dish_id)
             if not dish or not dish['availability']:
-                return jsonify(error='Invalid dish ID or dish not available')
+                return generate_response(error='Invalid dish ID or dish not available', status_code=400)
 
-        order_id = add_order(customer_name, dish_ids)
-        return jsonify(message='Order placed successfully', order_id=order_id)
+        order_id = add_order(customer_name, dish_ids, customer_email)
+        return generate_response(message='Order placed successfully', data={'order_id': order_id})
 
-
-# Route for updating the status of an order
 @app.route('/order/update_status', methods=['PUT'])
 def update_status():
     data = request.get_json()
     order_id = int(data['order_id'])
     status = data['status']
     if update_order_status(order_id, status):
-        return jsonify(message='Order status updated successfully')
+        return generate_response(message='Order status updated successfully')
     else:
-        return jsonify(error='Invalid order ID')
+        return generate_response(error='Invalid order ID', status_code=400)
 
-
-# Route for displaying all orders
 @app.route('/orders')
 def display_orders():
+    orders = list(orders_collection.find())
     orders_with_details = []
     for order in orders:
         order_with_details = {}
@@ -218,18 +160,20 @@ def display_orders():
         order_with_details['customer_name'] = order['customer_name']
         order_with_details['status'] = order['status']
 
-        # Fetch dish names and calculate total price
-        dish_names = [get_dish_by_id(dish_id)['name'] for dish_id in order['dish_ids']]
-        total_price = sum([int(get_dish_by_id(dish_id)['price']) for dish_id in order['dish_ids']])
+        dish_names = []
+        total_price = 0
+        for dish_id in order['dish_ids']:
+            dish = get_dish_by_id(dish_id)
+            if dish:
+                dish_names.append(dish['name'])
+                total_price += dish['price']
 
         order_with_details['dishes'] = dish_names
         order_with_details['total_price'] = total_price
 
         orders_with_details.append(order_with_details)
 
-    return jsonify(orders=orders_with_details)
-
-
+    return generate_response(data={'orders': orders_with_details})
 
 if __name__ == '__main__':
     app.run(debug=True)
